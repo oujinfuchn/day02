da1b76a403a1997556c144818047e3a6eef86da7
oujinfu <oujinfuchn@163.com>
2017-02-27 15:30:04 +0800

    修改结构2.0

--- Swift-day02/Swift-day02/ViewController/Main/MainViewController.swift
+++ Swift-day02/Swift-day02/ViewController/Main/MainViewController.swift
@@ -14,22 +14,37 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+//        addAllChildViewControllers(HomeViewController(), title: "首页", imageName: "", highImageName: "")
+//        addAllChildViewControllers(MessageViewController(), title: "消息", imageName: "", highImageName: "")
+//
+//        addAllChildViewControllers(DiscoverViewController(), title: "发现", imageName: "", highImageName: "")
+//
+//        addAllChildViewControllers(ProfileViewController(), title: "我的", imageName: "", highImageName: "")
+
+//        let cc  = 
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MainViewController{
+   
+    /// <#Description#>
+    ///
+    /// - parameter childController: <#childController description#>
+    /// - parameter title:           <#title description#>
+    /// - parameter imageName:       <#imageName description#>
+    /// - parameter highImageName:   <#highImageName description#>
+    ///
+    /// - returns: <#return value description#>
+    fileprivate func addAllChildViewControllers(_ childVC: UIViewController , title: String , imageName: String , highImageName: String) -> UIViewController {
+        
+        childVC.tabBarItem.title = title;
+        childVC.tabBarItem.image = UIImage.init(named: imageName);
+        childVC.tabBarItem.selectedImage = UIImage.init(named: highImageName);
+        let nav = NavigationController(rootViewController:childVC)
+        return nav;
     }
-    */
-
 }
